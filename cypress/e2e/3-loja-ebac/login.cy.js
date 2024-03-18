c48373e6789ed4858c4cf7b8fdc9fa82b1915672
ysnaraujo <yasaraujo@icloud.com>
2024-03-18 20:44:35 -0300

    :sparkles: testes de cenarios negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,12 +2,35 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Login com sucesso', () => {
         cy.get('#username').type('yasmin.teste@ebac.com.br')
         cy.get('#password').type('xofcis-Minru8-tyjcyf')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, yasmin.teste')
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('yasmin1.teste@ebac.com.br')
+        cy.get('#password').type('xofcis-Minru8-tyjcyf')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('yasmin.teste@ebac.com.br')
+        cy.get('#password').type('xofcis-Minru8-tyjcyf2')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('exist')
+    });
 })
